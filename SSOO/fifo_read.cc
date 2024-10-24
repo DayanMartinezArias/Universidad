b7e38006acb688f198c4eaab0396ccbf3696c654
Dayán Martínez <alu0101644561@ull.edu.es>
2024-10-24 22:37:19 +0100

    Práctica de Bash empezada

--- SSOO/fifo_read.cc
+++ SSOO/fifo_read.cc
@@ -5,7 +5,7 @@
 int main() {
     const char* path = "./mi_fifo";
     int fd = open(path, O_RDONLY);  // Abrir el FIFO en modo lectura
-
+    
     char buffer[100];
     read(fd, buffer, sizeof(buffer));  // Leer del FIFO
     std::cout << "Mensaje recibido: " << buffer << std::endl;
--- SSOO/infosession.sh
+++ SSOO/infosession.sh
@@ -1,21 +1,82 @@
 #!/bin/bash
 
-CORRECT_PARAMS=3
+PROGNAME=$(basename $0)
+USRS=""
 
 show_process() {
-  ps --no-header -eo sid,pgid,pid,user,tty,%mem,cmd --sort=user | awk '$1 != 0'
-}
-
-check_args() {
-  if [ "$#" -gt "$CORRECT_PARAMS" ]; then
-    echo "Incorrect number of params, please introduce a maximum of $CORRECT_PARAMS params"
-    exit 1
+  if [ "$USRS" == "" ]; then
+    ps ax -o sid,pgid,pid,user,tty,%mem,cmd --sort=user | awk '$1 != 0'
+  else
+    ps ax -o sid,pgid,pid,user,tty,%mem,cmd -u $USRS --sort=user | awk '$1 != 0'
   fi
 }
 
-# Llamar a check_args antes de mostrar los procesos
-check_args "$@"
+show_process_sid_zero() {
+  if [ "$USRS" == "" ]; then
+    ps ax -o sid,pgid,pid,user,tty,%mem,cmd --sort=user
+  else
+    ps ax -o sid,pgid,pid,user,tty,%mem,cmd -u$USRS --sort=user 
+  fi
+}
 
-cat << _EOF_
-$(show_process)
+show_help() {
+  cat << _EOF_
+Usage: $PROGNAME [OPTIONS]
+
+Options:
+  -h        Show this help message and exit
+  -z        Shows all processes with sid 0
+  -u user   Shows all processes of a specific user
 _EOF_
+}
+
+unknown_param() {
+  echo ERROR: An Unknown param was introduced: $1
+}
+
+check_help() {
+
+  for arg in "$@"; do
+    if [ "$arg" == "-h" ]; then
+      show_help
+      exit 0
+    fi
+  done
+
+}
+
+read_args() {
+  while [ "$1" != "" ]; do
+    case $1 in
+      -u )
+        if [[ "$2" == "" ]]; then
+          echo "Opción $1 requiere un argumento"
+          exit 1
+        fi
+        USRS=$2
+        shift
+        ;;
+      -z )
+        show_process_sid_zero
+        exit 0
+        ;;
+      * )
+        unknown_param $1
+        exit 1
+      esac
+      shift
+  done
+}
+
+check_help "$@"
+read_args "$@" 
+
+show_process 
+exit 0
+
+## COSAS A HECER ##
+
+# ERROR PARA COMANDOS DOBLES
+# ASEGURASE DE QUE EXISTE EL NOMBRE DE USUARIO
+# MEJORAR LA FORMA MOTRAR LOS PROCESOS POR USUARIO
+
